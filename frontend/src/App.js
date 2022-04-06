e1f1f92496df1afe133e908da70bd916d4b5c527
Gladimir Jacobsen <gladimirjacobsen@gmail.com>
2022-04-06 08:49:21 -0400

    Adding react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Header from './component/Header';
 function App() {
 	return (
-		<div>
-			<h1>Goal</h1>
-		</div>
+		<>
+			<Router>
+				<div className='container'>
+					<Header />
+					<Routes>
+						<Route element={<Dashboard />} path='/'></Route>
+						<Route element={<Login />} path='/login'></Route>
+						<Route element={<Register />} path='/register'></Route>
+					</Routes>
+				</div>
+			</Router>
+		</>
 	);
 }
 
