44c69264b798701615bcfbcfaf8aef4aca0626d0
Gladimir Jacobsen <gladimirjacobsen@gmail.com>
2022-04-09 23:02:53 -0400

    Adding registe service and reducer

--- frontend/src/features/Auth/authSlice.js
+++ frontend/src/features/Auth/authSlice.js
@@ -1,6 +1,35 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 const user = JSON.parse(localStorage.getItem('user'));
+
+const API_URL = '/api/users/';
+
+const registerUser = async (userData) => {
+	const res = await axios.post(API_URL, userData);
+	if (res.data) {
+		localStorage.setItem('user', JSON.stringify(res.data));
+	}
+
+	return res.data;
+};
+export const register = createAsyncThunk(
+	'auth/register',
+	async (user, thunkApi) => {
+		try {
+			return await registerUser(user);
+		} catch (error) {
+			const message =
+				(error.response &&
+					error.response.data &&
+					error.response.data.message) ||
+				error.message ||
+				error.toString();
+
+			return thunkApi.rejectWithValue(message);
+		}
+	}
+);
 
 const initialState = {
 	user: user ? user : null,
@@ -21,7 +50,24 @@ const authSlice = createSlice({
 			state.message = '';
 		},
 	},
-	extraReducers: (state) => {},
+	extraReducers: (builder) => {
+		builder
+			.addCase(register.pending, (state) => {
+				state.isLoading = true;
+			})
+			.addCase(register.fulfilled, (state, action) => {
+				state.isLoading = false;
+				state.user = action.paylaod;
+				state.isSuccess = true;
+			})
+			.addCase(register.rejected, (state, action) => {
+				state.user = null;
+				state.isError = true;
+				state.isLoading = false;
+				state.isSuccess = false;
+				message = action.paylaod;
+			});
+	},
 });
 
 export const { reset } = authSlice.actions;
